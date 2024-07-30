12cfa41f516d3b4fff76ca15a7ded2d76e206cf6
Harishsm20 <harishsm865@gmail.com>
2024-07-30 22:05:17 +0530

    Credentials secured

--- server/server.js
+++ server/server.js
@@ -2,22 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 require('dotenv').config();
+const session = require('express-session'); 
+const dotenv = require('dotenv');
 
 const app = express();
 const authController = require('./controllers/authController'); 
 const flightController = require('./controllers/flightController');
 const bookController = require('./controllers/bookingController');
 
-const session = require('express-session'); 
-
 const PORT = 3001;
 
 // Connect to MongoDB
 const connect = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Harish:IpcoahXz8yD2IibT@cluster0.p3yw9uj.mongodb.net/employee', {
+    await mongoose.connect(`mongodb+srv://${process.env.User}:${process.env.pswd}@cluster0.p3yw9uj.mongodb.net/${process.env.DB}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -29,24 +28,26 @@ const connect = async () => {
 
 connect();
 
-app.use(cors(
-  { origin: 'http://localhost:5173' ,
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(bodyParser.json());
 
 app.use(session({
   secret: 'placeholder_secret', // This will be overridden per session
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } 
+  cookie: { secure: false },
 }));
 
 // Use controllers for routing
 app.use('/auth', authController); 
 app.use('/airlines', flightController); 
-app.use('./book',bookController);
+app.use('/book', bookController);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
